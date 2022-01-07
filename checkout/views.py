c3d27cbc7dfc94437650edd2e42b9c5066b59a8a
MichelleCoffey <michellembcoffey@icloud.com>
2022-01-07 13:41:40 +0000

    started stripe and added stripee card css.

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51Jo1l7GqHuyYmd1vmd6cYBAE428f8Hm1Lz2vyyfbjQh1psTJlYLml6gEDSC3j5NpxygvCkW1n4JnlcOq916TeSEz00Gta8jaZP',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
