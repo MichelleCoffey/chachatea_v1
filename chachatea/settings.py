15e24e35be991509d7d7bad657b89427e94ebedf
MichelleCoffey <michellembcoffey@icloud.com>
2022-01-23 11:08:15 +0000

    Changed secret key.

--- chachatea/settings.py
+++ chachatea/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-m%)a^vc0hx79=8ki%&g!#ub0t&um)z_p999$xx1ho0(&5fd774'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['chachatea.herokuapp.com', 'localhost']
 
