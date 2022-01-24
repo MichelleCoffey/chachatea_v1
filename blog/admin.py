a067a1118fb6852a4579f05f92eb71626547a54c
MichelleCoffey <michellembcoffey@icloud.com>
2022-01-24 08:57:15 +0000

    added summernote to edit blog posts.

--- blog/admin.py
+++ blog/admin.py
@@ -1,12 +1,16 @@
 from django.contrib import admin
 from .models import Post
+from django_summernote.admin import SummernoteModelAdmin
 
 
-class PostAdmin(admin.ModelAdmin):
+@admin.register(Post)
+class PostAdmin(SummernoteModelAdmin):
+
     list_display = ('title', 'slug', 'status', 'created_on')
     list_filter = ("status",)
     search_fields = ['title', 'content']
     prepopulated_fields = {'slug': ('title',)}
- 
+    summernote_fields = ('content')
 
-admin.site.register(Post, PostAdmin)
+
+
